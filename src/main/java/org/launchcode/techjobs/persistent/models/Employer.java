925403156b5d6576f33a5c5b0a8d9583d1d5266d
Muhammad Qayyum <msqgrewal123@gmail.com>
2023-11-12 16:51:44 -0600

    Models Task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,33 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(min =3, max = 150, message = "Invalid name. Must be between 3 and 150 characters.")
+    private String location;
+
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+    @Size(min =3, max = 500, message = "Invalid name. Must be between 3 and 500 characters.")
+    private String description;
+
+    public Skill() {}
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 
 }
