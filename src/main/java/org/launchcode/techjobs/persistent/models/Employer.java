1ca393a786bff22c4ee312acd76f6156a00d7fd9
Muhammad Qayyum <msqgrewal123@gmail.com>
2023-11-12 18:00:43 -0600

    Add jobs and Job Model Task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,6 +15,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(min =3, max = 150, message = "Invalid name. Must be between 3 and 150 characters.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
 
     public Employer(String location) {
